796294c9eb6755c2a6c7a98ba7b0223d302763e0
Sordidez <1972227689@qq.com>
2022-02-28 10:05:30 +0800

    first commit

---
+++ src/main/java/com/sordidez/gitTest.java
@@ -0,0 +1,14 @@
+package com.sordidez;
+
+/**
+ * Copyright (C), 2017-2022, Sordidez
+ * Author: sordidez
+ * Date: 2022/2/28 10:02
+ * FileName: gitTest
+ * Description: ${DESCRIPTION}
+ */
+public class gitTest {
+    public static void main(){
+        System.out.println("git test1");
+    }
+}
